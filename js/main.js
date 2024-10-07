ffbbf1b19785bb3298a07df340da4d098e7cdfa3
Namania <mael.namania@gmail.com>
2024-10-07 19:19:17 +0200

    :pencil: password for startup

---
+++ js/main.js
@@ -0,0 +1,25 @@
+private_password = "230949293aef1c98eade64ac412bacd40d5d728920134be8a406ffe75d81beaa"
+
+const startup_btn = document.getElementById("startup-btn");
+startup_btn.addEventListener("click", () => {
+    hash(prompt("Enter password", "")).then((sha) => {
+        if (sha == private_password) {
+            console.log("Good password");
+            document.getElementById("startup-btn").hidden = true;
+            document.getElementById("download-startup").hidden = false;
+        } else {
+            console.log("Bad password");
+        }
+    });
+});
+
+function hash(string) {
+    const utf8 = new TextEncoder().encode(string);
+    return crypto.subtle.digest('SHA-256', utf8).then((hashBuffer) => {
+        const hashArray = Array.from(new Uint8Array(hashBuffer));
+        const hashHex = hashArray
+            .map((bytes) => bytes.toString(16).padStart(2, '0'))
+            .join('');
+        return hashHex;
+    });
+}
